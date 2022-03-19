0f9d2c0976a7754716130efa048566f34d5c6e1c
ahmed sayed <58530571+ahmedsayed621@users.noreply.github.com>
2022-03-19 16:42:12 +0200

    Intial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MoviesComponent } from './movies/movies.component';
+import { NetworkComponent } from './network/network.component';
+import { TvComponent } from './tv/tv.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { PeopleComponent } from './people/people.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    MoviesComponent,
+    NetworkComponent,
+    TvComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    NotfoundComponent,
+    PeopleComponent
   ],
   imports: [
     BrowserModule,
